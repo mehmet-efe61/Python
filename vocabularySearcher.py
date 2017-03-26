7a85da061a38cde6f2a7085a277e37af9efb0817
mehmet-efe61 <mehmetefekaradeniz.mek@gmail.com>
2017-03-26 23:05:28 +0300

    Latest

---
+++ vocabularySearcher.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+import sys
+while True:
+    a=str(input("Kelimeyi girin\n"))
+    for i in range(4):
+        print("Loading" + "." * i)
+        sys.stdout.write("\033[F")
+        time.sleep(.200)
+    webbrowser.open("http://dictionary.cambridge.org/dictionary/turkish/"+a)
+    webbrowser.open("http://www.thesaurus.com/browse/"+a+"?s=t")
+    webbrowser.open("http://zargan.com/tr/q/"+a+"-ceviri-nedir")
