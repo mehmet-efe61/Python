ecc221e7a57adc702667bf9aa51a404ae13982f4
mehmet-efe61 <mehmetefekaradeniz.mek@gmail.com>
2017-03-27 17:32:42 +0300

    Latest

--- vocabularySearcher.py
+++ vocabularySearcher.py
@@ -3,10 +3,13 @@ import webbrowser
 import sys
 while True:
     a=str(input("Kelimeyi girin\n"))
-    for i in range(4):
-        print("Loading" + "." * i)
-        sys.stdout.write("\033[F")
-        time.sleep(.200)
-    webbrowser.open("http://dictionary.cambridge.org/dictionary/turkish/"+a)
-    webbrowser.open("http://www.thesaurus.com/browse/"+a+"?s=t")
-    webbrowser.open("http://zargan.com/tr/q/"+a+"-ceviri-nedir")
+    if a  == "exit":
+        sys.exit()
+    else:
+        for i in range(4):
+            print("Loading" + "." * i)
+            sys.stdout.write("\033[F")
+            time.sleep(.200)
+        webbrowser.open("http://dictionary.cambridge.org/dictionary/turkish/"+a)
+        webbrowser.open("http://www.thesaurus.com/browse/"+a+"?s=t")
+        webbrowser.open("http://zargan.com/tr/q/"+a+"-ceviri-nedir")
