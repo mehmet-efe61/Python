009f2e7bfd8381db7d8e7c440aff64aa601a35b3
mehmet-efe61 <mehmetefekaradeniz.mek@gmail.com>
2017-05-29 20:17:06 +0300

    Latest

--- vocabularySearcher.py
+++ vocabularySearcher.py
@@ -7,22 +7,25 @@ from bs4 import BeautifulSoup
 import os
 
 def translate():
-    url="http://dictionary.cambridge.org/dictionary/"+lang+"/"+word
+    url="http://dictionary.cambridge.org/dictionary/english-"+lang+"/"+word
     page=urllib.request.urlopen(url)
     soup=BeautifulSoup(page,"html.parser")
-    print((soup.find('span',{"class":"trans"}).text).strip())
+    print(lang+": "+(soup.find('span',{"class":"trans"}).text).strip())
 
 word=str(input("Kelimeyi girin\n"))
 os.system("say "+word+" -v daniel")
+
 lang="turkish"
-translate()
-lang="english-german"
-translate()
-lang="english-spanish"
-translate()
-lang="english-french"
+url="http://dictionary.cambridge.org/dictionary/"+lang+"/"+word
+page=urllib.request.urlopen(url)
+soup=BeautifulSoup(page,"html.parser")
+print(lang+": "+(soup.find('span',{"class":"trans"}).text).strip())
+
+lang="german"
 translate()
 
+lang="spanish"
+translate()
 
-
-
+lang="french"
+translate()
