2dbf4cad810ae029836c31a8641ce1489b4bf840
mehmet-efe61 <mehmetefekaradeniz.mek@gmail.com>
2017-05-29 20:08:02 +0300

    Latest

--- TelegramBot/orhunBot.py
+++ TelegramBot/orhunBot.py
@@ -42,7 +42,7 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 def send_message(text, chat_id):
-    text = "anan"
+    text = "ağla"
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
--- vocabularySearcher.py
+++ vocabularySearcher.py
@@ -1,15 +1,28 @@
 import time
 import webbrowser
 import sys
-while True:
-    a=str(input("Kelimeyi girin\n"))
-    if a  == "exit":
-        sys.exit()
-    else:
-        for i in range(4):
-            print("Loading" + "." * i)
-            sys.stdout.write("\033[F")
-            time.sleep(.200)
-        webbrowser.open("http://dictionary.cambridge.org/dictionary/turkish/"+a)
-        webbrowser.open("http://www.thesaurus.com/browse/"+a+"?s=t")
-        webbrowser.open("http://zargan.com/tr/q/"+a+"-ceviri-nedir")
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+
+def translate():
+    url="http://dictionary.cambridge.org/dictionary/"+lang+"/"+word
+    page=urllib.request.urlopen(url)
+    soup=BeautifulSoup(page,"html.parser")
+    print((soup.find('span',{"class":"trans"}).text).strip())
+
+word=str(input("Kelimeyi girin\n"))
+os.system("say "+word+" -v daniel")
+lang="turkish"
+translate()
+lang="english-german"
+translate()
+lang="english-spanish"
+translate()
+lang="english-french"
+translate()
+
+
+
+
